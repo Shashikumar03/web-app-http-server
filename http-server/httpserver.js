13a3e3299bb9e8ddf9eee863bf74bf3ffab2d3d4
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-12-09 10:52:26 +0530

    refractor header

--- http-server/httpserver.js
+++ http-server/httpserver.js
@@ -22,10 +22,10 @@ const server = http.createServer((req, res) => {
     case "/html":
       fs.readFile("http-server/index.html", "utf-8", (err, data) => {
         if (err) {
-          res.writeHead(500, { "content-type": "text/plain" });
+          res.writeHead(500, { "Content-Type": "text/plain" });
           res.end("page Not found with error status: 500 ");
         } else {
-          res.writeHead(200, { "content-type": "text/html" });
+          res.writeHead(200, { "Content-Type": "text/html" });
           res.end(data);
         }
       });
@@ -40,12 +40,12 @@ const server = http.createServer((req, res) => {
       res.end(json);
       break;
     case `/status/${code}`:
-      res.writeHead(code, { "content-type": "text/plain" });
+      res.writeHead(code, { "Content-Type": "text/plain" });
       res.end(`status code- ${code}`);
       break;
     case `/delay/${code}`:
       setTimeout(() => {
-        res.writeHead(200, { "content-type": "text/plain" });
+        res.writeHead(200, { "Content-Type": "text/plain" });
         res.end(`a delay of ${code} sec`);
       }, code * 1000);
       break;
